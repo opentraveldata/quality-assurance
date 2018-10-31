e3ea57298c8a9aed3fd222ecb46ea8abea2e9912
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2018-10-31 22:22:27 +0100

    [POR] Added a script to check UN/LOCODE POR

---
+++ checkers/check-por-cmp-optd-unlc.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python3
+
+import csv, datetime
+import DeliveryQuality as dq
+
+# Main
+if __name__ == '__main__':
+  #
+  usageStr = "That script downloads OpenTravelData (OPTD) airline-related CSV files\nand check that the UN/LOCODE POR are present in Geonames (and in the OPTD POR file)"
+  verboseFlag = dq.handle_opt(usageStr)
+
+  # OPTD-maintained list of UN/LOCODE-referenced POR
+  optd_por_extd_unlc_url = 'https://github.com/opentraveldata/opentraveldata/blob/master/opentraveldata/optd_por_unlc.csv?raw=true'
+  optd_por_extd_unlc_file = 'to_be_checked/optd_por_unlc.csv'
+
+  # UN/LOCODE derived list of POR
+  optd_por_unlc_url = 'https://github.com/opentraveldata/opentraveldata/blob/master/data/unlocode/archives/unlocode-code-list-2018-1.csv?raw=true'
+  optd_por_unlc_file = 'to_be_checked/unlocode-code-list-latest.csv'
+
+  # If the files are not present, or are too old, download them
+  dq.downloadFileIfNeeded (optd_por_extd_unlc_url, optd_por_extd_unlc_file, verboseFlag)
+  dq.downloadFileIfNeeded (optd_por_unlc_url, optd_por_unlc_file, verboseFlag)
+
+  # DEBUG
+  if verboseFlag:
+    dq.displayFileHead (optd_por_extd_unlc_file)
+    dq.displayFileHead (optd_por_unlc_file)
+
+  # OPTD-maintained POR with the full details
+  optd_por_dict = dict()
+  with open (optd_por_extd_unlc_file, newline='') as csvfile:
+    file_reader = csv.DictReader (csvfile, delimiter='^')
+    for row in file_reader:
+      por_code = row['unlc']
+      optd_fclass = row['feat_class']
+      optd_fcode = row['feat_code']
+      optd_geo_id = row['geonames_id']
+      optd_coord_lat = row['latitude']
+      optd_coord_lon = row['longitude']
+
+      #
+      if not por_code in optd_por_dict or optd_por_dict[por_code] != '':
+        # Register the OPTD details for the POR
+        optd_por_dict[por_code] = {'por_code': por_code,
+                                   'fclass': optd_fclass,
+                                   'fcode': optd_fcode,
+                                   'geo_lat': optd_coord_lat,
+                                   'geo_lon': optd_coord_lon,
+                                   'geo_id': optd_geo_id}
+
+  # UN/LOCODE derived list of POR
+  unlc_por_dict = dict()
+  with open (optd_por_unlc_file, newline='') as csvfile:
+    file_reader = csv.DictReader (csvfile, delimiter='^')
+    for row in file_reader:
+      unlc_por_code = row['por_code']
+      #unlc_por_name = row['por_name']
+      it_loc_type = row['loc_type']
+      it_state_code = row['state_code']
+      it_ctry_code = row['country_code']
+      it_cty_code = row['city_code']
+      #it_cty_name = row['city_name']
+      #it_tz_code = row['tz_code']
+      #it_loc_id = row['loc_id']
+
+      # Check whether the POR is known to be an exception
+      # for the reference ("I") source
+      unlc_por_exc = False
+      if unlc_por_code in por_exc_dict:
+        if "I" in por_exc_dict[unlc_por_code]['source'] and por_exc_dict[unlc_por_code]['actv_in_optd'] == "0" and por_exc_dict[unlc_por_code]['actv_in_src'] == "1":
+          unlc_por_exc = True
+
+          # Remove the record from the known errors, as it has been handled
+          por_exc_dict[unlc_por_code]['used'] = True
+
+      # Check whether the UN/LOCODE derived POR is in the list of OPTD POR
+      if not unlc_por_code in optd_por_dict and not it_cty_code in optd_por_dict and not unlc_por_exc:
+        # The OPTD POR cannot be found in the list of UN/LOCODE derived POR,
+        # and it is not a known exception
+        reasonStr = "UN/LOCODE derived POR not in OpenTravelData"
+        reportStruct = {'por_code': unlc_por_code, 'in_optd': 0, 'in_unlc': 1,
+                        'reason': reasonStr}
+        print (str(reportStruct))
+
+      elif not unlc_por_exc:
+        # Still not a known exception, but the UN/LOCODE POR is known by OPTD,
+        # either as a travel-related POR or as a city POR:
+        # if unlc_por_code in optd_por_dict or it_cty_code in optd_por_dict
+
+        it_code = unlc_por_code
+        if unlc_por_code in optd_por_dict:
+          # First, register the UN/LOCODE derived POR, so that we can then,
+          # in the next main loop, search for OPTD POR not referenced by UN/LOCODE
+          unlc_por_dict[unlc_por_code] = {'loc_type': it_loc_type,
+                                      'state_code': it_state_code,
+                                      'country_code': it_ctry_code,
+                                      'city_code': it_cty_code}
+        if it_cty_code in optd_por_dict:
+          # If the city code is different from the travel related code,
+          # and that city code is still not known from OPTD, register it
+          unlc_por_dict[it_cty_code] = {'loc_type': "C",
+                                      'state_code': it_state_code,
+                                      'country_code': it_ctry_code,
+                                      'city_code': it_cty_code}
+          # Only the UN/LOCODE city code is known by OPTD
+          if not unlc_por_code in optd_por_dict:
+            it_code = it_cty_code
+
+        # The OPTD POR is in the list of UN/LOCODE derived POR,
+        # and it is not a known exception: retrieve the details from OPTD
+        optd_por_details = optd_por_dict[it_code]
+
+        optd_env_id = optd_por_details['env_id']
+        optd_date_from = optd_por_details['date_from']
+        optd_date_until = optd_por_details['date_until']
+        optd_state_code = optd_por_details['state_code']
+        optd_ctry_code = optd_por_details['ctry_code']
+        optd_cty_list_str = optd_por_details['cty_list']
+        optd_loc_type = optd_por_details['loc_type']
+        optd_geo_id = optd_por_details['geo_id']
+        optd_feat_class = optd_por_details['feat_class']
+        optd_feat_code = optd_por_details['feat_code']
+        optd_page_rank = optd_por_details['page_rank']
+
+        if optd_env_id != '' and it_ctry_code != optd_ctry_code:
+          # The OPTD POR is no longer valid, whereas still active
+          # in the list of UN/LOCODE derived POR
+          reasonStr = "UN/LOCODE derived POR no longer valid in OpenTravelData"
+          reportStruct = {'por_code': unlc_por_code, 'in_optd': 1, 'in_unlc': 1,
+                          'env_id': optd_env_id,
+                          'date_from': optd_date_from,
+                          'date_until': optd_date_until,
+                          'it_state_code': it_state_code,
+                          'it_ctry_code': it_ctry_code,
+                          'it_cty_code': it_cty_code,
+                          'it_loc_type': it_loc_type,
+                          'optd_geo_id': optd_geo_id,
+                          'optd_state_code': optd_state_code,
+                          'optd_ctry_code': optd_ctry_code,
+                          'optd_cty_list': optd_cty_list_str,
+                          'optd_loc_type': optd_loc_type,
+                          'optd_feat_class': optd_feat_class,
+                          'optd_feat_code': optd_feat_code,
+                          'optd_page_rank': optd_page_rank,
+                          'reason': reasonStr}
+          print (str(reportStruct))
+
+        if optd_env_id == '' and not unlc_por_code in optd_por_dict:
+          # Only the UN/LOCODE city code is known by OPTD
+          reasonStr = "UN/LOCODE derived POR in OpenTravelData as a city, but not as travel-related POR"
+          reportStruct = {'por_code': unlc_por_code, 'in_optd': 1, 'in_unlc': 1,
+                          'env_id': optd_env_id,
+                          'date_from': optd_date_from,
+                          'date_until': optd_date_until,
+                          'it_state_code': it_state_code,
+                          'it_ctry_code': it_ctry_code,
+                          'it_cty_code': it_cty_code,
+                          'it_loc_type': it_loc_type,
+                          'optd_geo_id': optd_geo_id,
+                          'optd_state_code': optd_state_code,
+                          'optd_ctry_code': optd_ctry_code,
+                          'optd_cty_list': optd_cty_list_str,
+                          'optd_loc_type': optd_loc_type,
+                          'optd_feat_class': optd_feat_class,
+                          'optd_feat_code': optd_feat_code,
+                          'optd_page_rank': optd_page_rank,
+                          'reason': reasonStr}
+          print (str(reportStruct))
+
+        # ISO 3166-2 state code
+        if it_state_code != optd_state_code:
+          # The state codes are not the same for the UN/LOCODE- and OPTD-derived POR
+          
+          # First, check that there is no known exception
+          is_state_exc = False
+
+          # In the optd_state_exceptions.csv
+          full_state_code = dq.getFullStateCode (optd_ctry_code, optd_state_code)
+          if full_state_code in state_exc_dict:
+            state_exc_details = state_exc_dict[full_state_code]
+            state_exc_geo_id = state_exc_details['geo_id']
+            state_exc_wrong_state_code = state_exc_details['wrong_state_code']
+            if it_state_code == state_exc_wrong_state_code:
+              is_state_exc = True
+              state_exc_dict[full_state_code]['used'] = True
+
+          # In the optd_por_exceptions.csv
+          optd_por_exc_state_diff = it_code in por_exc_dict and "I" in por_exc_dict[it_code]['source'] and por_exc_dict[it_code]['actv_in_optd'] == "1" and por_exc_dict[it_code]['actv_in_src'] == "1"
+          if optd_por_exc_state_diff:
+            is_state_exc = True
+            por_exc_dict[it_code]['used'] = True
+
+          #
+          if not is_state_exc:
+            # No known exception rule applies for that state
+            reasonStr = "UN/LOCODE- and OPTD-derived POR have not the same state codes"
+            reportStruct = {'por_code': unlc_por_code, 'in_optd': 1, 'in_unlc': 1,
+                            'env_id': optd_env_id,
+                            'date_from': optd_date_from,
+                            'date_until': optd_date_until,
+                            'it_state_code': it_state_code,
+                            'it_ctry_code': it_ctry_code,
+                            'it_cty_code': it_cty_code,
+                            'it_loc_type': it_loc_type,
+                            'optd_geo_id': optd_geo_id,
+                            'optd_state_code': optd_state_code,
+                            'optd_ctry_code': optd_ctry_code,
+                            'optd_cty_list': optd_cty_list_str,
+                            'optd_loc_type': optd_loc_type,
+                            'optd_feat_class': optd_feat_class,
+                            'optd_feat_code': optd_feat_code,
+                            'optd_page_rank': optd_page_rank,
+                            'reason': reasonStr}
+            print (str(reportStruct))
+
+      else:
+        # The exception rule has been handled; register it
+        por_exc_dict[unlc_por_code]['used'] = True
+
+  # Search for OPTD POR not, or no longer, referenced by UN/LOCODE
+  for optd_por_code in optd_por_dict:
+    optd_por_details = optd_por_dict[optd_por_code]
+    optd_env_id = optd_por_details['env_id']
+
+    # Check whether there is an exception rule
+    optd_por_exc_in_optd_but_not_it = optd_por_code in por_exc_dict and "I" in por_exc_dict[optd_por_code]['source'] and por_exc_dict[optd_por_code]['actv_in_optd'] == "1" and por_exc_dict[optd_por_code]['actv_in_src'] == "0"
+
+    if not optd_por_code in unlc_por_dict and optd_env_id == "" and not optd_por_exc_in_optd_but_not_it:
+      # Retrieve the details from OPTD
+      optd_state_code = optd_por_details['state_code']
+      optd_ctry_code = optd_por_details['ctry_code']
+      optd_cty_list_str = optd_por_details['cty_list']
+      optd_loc_type = optd_por_details['loc_type']
+      optd_geo_id = optd_por_details['geo_id']
+      optd_feat_class = optd_por_details['feat_class']
+      optd_feat_code = optd_por_details['feat_code']
+      optd_page_rank = optd_por_details['page_rank']
+
+      reasonStr = "OPTD POR not/no longer referenced by UN/LOCODE"
+      reportStruct = {'por_code': optd_por_code, 'in_optd': 1, 'in_unlc': 0,
+                      'optd_geo_id': optd_geo_id,
+                      'optd_state_code': optd_state_code,
+                      'optd_ctry_code': optd_ctry_code,
+                      'optd_cty_list': optd_cty_list_str,
+                      'optd_loc_type': optd_loc_type,
+                      'optd_feat_class': optd_feat_class,
+                      'optd_feat_code': optd_feat_code,
+                      'optd_page_rank': optd_page_rank,
+                      'reason': reasonStr}
+      print (str(reportStruct))
+  
+  # Sanity checks
+  # All the (POR, city)-related exception rules should have been used
+  for por_code_exc in por_exc_dict:
+    if not por_exc_dict[por_code_exc]['used'] and por_exc_dict[por_code_exc]['actv_in_src'] == "1":
+      reportStruct = {'por_code': por_code_exc,
+                      'actv_in_it': por_exc_dict[por_code_exc]['actv_in_src']}
+      print ("!!!!! Remaining entry of the file of (POR, city)-related related known exceptions: " + str(reportStruct) + ". Please, remove that from the '" + optd_por_exc_url + "' file.")
+
+  # All the state-related exception rules should have been used
+  for state_code_exc in state_exc_dict:
+    if not state_exc_dict[state_code_exc]['used']:
+      reportStruct = {'full_state_code': state_code_exc,
+                      'wrong_state_code': state_exc_dict[state_code_exc]['wrong_state_code']}
+      print ("!!!!! Remaining entry of the file of state-related known exceptions: " + str(reportStruct) + ". Please, remove that from the '" + optd_state_exc_url + "' file.")
+    
+
+  # DEBUG
+  if verboseFlag:
+    print ("OPTD POR data full dictionary:\n" + str(optd_por_dict))
