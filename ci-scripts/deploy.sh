00e0aaa8449266f2f93354ee625f02b6a9fbc285
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2019-12-16 02:12:53 +0100

    [CI] The host keys should be injected in know_hosts

--- ci-scripts/deploy.sh
+++ ci-scripts/deploy.sh
@@ -16,11 +16,17 @@ export DATA_DIR="${DATA_DIR_BASE}/${TODAY_DATE}"
 echo "TODAY_DATE=${TODAY_DATE} - DATA_DIR=${DATA_DIR}"
 
 #
-echo "Injecting a few host keys into ~/.ssh/config"
-cat >> ~/.ssh/config << _EOF
+echo "Injecting a few host keys into ~/.ssh/known_hosts"
+cat >> ~/.ssh/known_hosts << _EOF
 www2-int2.transport-search.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBDmslzunyRnmtrJSwaP1vGuS+vTFBoodZRY1Ri+VIXR8qBKa4MGNgX5WfwQIEOCsbme4gzJ4BZHFNY8WAwNl500=
 www-int2.transport-search.org ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKuH70tG6Ep2ibfqkZMnhPhXan9uIEXuQXUMdNG7N8ZSTv713tO1moU/nVl/drrN68Z4bLD+Nj49OIhj/9OM/W8=
 _EOF
+chmod 600 ~/.ssh/known_hosts
+
+#
+echo "Content of ~/.ssh/config"
+cat ~/.ssh/config
+echo "--"
 
 #
 echo "Creating ${DATA_DIR} on to qa@tits"
