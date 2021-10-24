72fb9dbbda18e861d860de6fa952f9324ebe1fe2
Denis Arnaud <denis.arnaud@decathlon.com>
2021-10-25 01:34:38 +0200

    Suppressed opentracing from the Python dependencies

--- test/es-tracing-test.py
+++ test/es-tracing-test.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 # https://github.com/opentracing-contrib/python-elasticsearch
-
+# Note: opentracing apparently requires futures, which does not work
+# on Python 3 (it is a backport on Python 2). A way to make it work is
+# to require, in the Pipfile:
+# futures = "==3.1.1"
 import elasticsearch_opentracing
 
 elasticsearch_opentracing.init_tracing(tracer) # An OpenTracing compatible tracer.
